f2f8aa6b6d39a6d230a835e6065c74eacb6b1008
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-08-02 20:49:12 +0200

    add os head

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
+import os
+os.environ['KIVY_NO_CONSOLELOG'] = '1'
+cwd = os.getcwd()
+print(cwd)
+os.environ['KIVY_HOME'] = cwd + '/conf'
+
+
+# import sqlite3
+
 from kivy.lang import Builder
 from kivymd.app import MDApp
-
 
 class TestNavigationDrawer(MDApp):
 
     def build(self):
+        print('Build 0')
+        
+        # self.create_db()
+
+
         return Builder.load_file('kv/main.kv')
+    
+    '''def create_db(self):
+        # print('CREATE START DB')
+        # Create Database Or Connect To One
+        conn = sqlite3.connect('coffe_app.db')
+        cur = conn.cursor()
+        sql = """CREATE TABLE if not exists act_tokens(
+            id INT PRIMARY KEY NOT NULL,
+            act_token TEXT,
+            act_expiry TEXT)"""
+        cur.execute(sql)
+        sql = """INSERT OR IGNORE INTO 
+                act_tokens (id, act_token) VALUES (?, ?)"""
+        data1 = (1,'a1')    
+        cur.execute(sql, data1)
+        conn.commit()
+
+        sql = """CREATE TABLE if not exists act_users(
+            id INT PRIMARY KEY NOT NULL,
+            act_user TEXT,
+            act_pass TEXT)"""
+        cur.execute(sql)
+        sql = """INSERT OR IGNORE INTO 
+                act_users (id, act_user, act_pass) 
+                VALUES (?, ?, ?)"""
+        data2 = (1,'a@a.com','abc123')    
+        cur.execute(sql, data2)
+        conn.commit()
+        conn.close()'''
 
 if __name__ == '__main__':
     print('START MAIN')
