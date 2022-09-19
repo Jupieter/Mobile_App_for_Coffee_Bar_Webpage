1037a019d404cfdb983f48fa011abcc4a1431b17
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-09-19 12:42:02 +0200

    service readded  request

--- service/service.py
+++ service/service.py
@@ -2,31 +2,32 @@ from time import sleep
 import requests
 from jnius import autoclass
 
-PythonService = autoclass('org.kivy.android.PythonService')
-print("PythonService")
-PythonService.mService.setAutoRestartService(True)
+# PythonService = autoclass('org.kivy.android.PythonService')
+# print("PythonService")
+# PythonService.mService.setAutoRestartService(True)
 
-# def load_data():
-#     try:
-#         store = requests.get('https://coffeeanteportas.herokuapp.com/c_app/todaytcoffee/').json()
-#         # print('STORE',store)
-#         if store == []:
-#             print('No coffee today')
-#         else:
-#             list_data = []
-#             for item in store:
-#                 list_data.append({'date': item['c_make_date'], "pkey": item['id']})
-#             first_coffe = list_data[0]['date']
-#             first_id = list_data[0]['pkey']
-#             dt = first_coffe[0:10]+' ' + first_coffe[11:19]
-#             print(first_id, ' Next Coffee:  ', dt)
-#             return dt
-#     except:
-#         print('Problem with internet conection')
-# 
+def load_data():
+    try:
+        store = requests.get('https://coffeeanteportas.herokuapp.com/c_app/todaytcoffee/').json()
+        # print('STORE',store)
+        if store == []:
+            dt = 'No coffee today'
+        else:
+            list_data = []
+            for item in store:
+                list_data.append({'date': item['c_make_date'], "pkey": item['id']})
+            first_coffe = list_data[0]['date']
+            first_id = list_data[0]['pkey']
+            dt = first_coffe[0:10]+' ' + first_coffe[11:19]
+            print(first_id, ' Next Coffee:  ', dt)
+        return dt
+    except:
+        dt = 'Problem with internet conection'
+        return dt
+
 
 while True:
     print("Coffeebar  service running.....")
-    # dt = load_data()
-    # print(dt)
-    sleep(8)
+    dt = load_data()
+    print(dt)
+    sleep(5)
