1a78c25d281f8f20d21a742365d421294012ac23
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-09-28 19:55:39 +0200

    bug h2

--- service/main.py
+++ service/main.py
@@ -4,11 +4,10 @@ def start_service():
     # service = autoclass("org.jupieter.coffee_ante.ServiceCoffeebar")
     mActivity = autoclass("org.kivy.android.PythonActivity").mActivity
     # service.start(mActivity, "")
-    # PythonActivity = autoclass('org.kivy.android.PythonActivity')
+    PythonActivity = autoclass('org.kivy.android.PythonActivity')
     # currentActivity = cast('android.app.Activity', PythonActivity.mActivity)
-    # context = cast('android.content.Context', currentActivity.getApplicationContext())
+    context = cast('android.content.Context', mActivity.getApplicationContext())
     job_service = autoclass("org.jupieter.coffee_ante.Util")
-    context = mActivity.getApplicationContext()
     job_service.scheduleJob(context)
     print("4 - start_service")
     # return job_service
